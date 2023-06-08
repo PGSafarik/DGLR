9002379c4dc78c82abe50932e9b005e7a8b0781d
D.A.Tiger <drakarax@seznam.cz>
2023-06-09 01:16:52 +0200

    re-implement go.sh

--- project/selfexec/Data/.tools/go.sh
+++ project/selfexec/Data/.tools/go.sh
@@ -1,13 +1,31 @@
 #! /bin/bash
+
 DOSBOX_EXEC=$(which dosbox)
 DOSBOX_BASE=${GAME_ROOT}/${DRIVE_DIR}/dosbox.conf
 DOSBOX_AUTOEXEC=${GAME_ROOT}/autoexec.conf
 
-GAME_EXEC="DEFAULT.EXE"
-GAME_CONFIG=""
+GAME_EXEC_FILE="DEFAULT.EXE"
+GAME_CONFIG_FILE="CONFIGURE.EXE"
+MANUAL_EXEC_FILE="MAUAL.EXE"
+
+_EDITOR=""
 
 ### Pomocne funkce ###
-source .scripts/utils.sh
+function check_editor( )
+{
+  if [ -n "$VISUAL" ]; then
+    _EDITOR=$VISUAL
+  else
+    if [ -n "$EDITOR" ]; then    
+      _EDITOR=$EDITOR
+    else
+      _EDITOR=$(which nano)   
+   fi	
+ fi
+   
+ vmessage "Detect text editor: $_EDITOR"
+   
+}
 
 function autoexec_gen( )
 {
@@ -21,52 +39,86 @@ MOUNT C $DRIVE_DIR
 C:
 CLS
 $1
-EXIT
+# EXIT
 EOF
-
 }
 
 function dosbox_run( )
 {
   autoexec_gen $1
-  dosbox -conf $DOSBOX_BASE -conf $DOSBOX_AUTOEXEC
+  
+  export SDL_MOUSE_RELATIVE=1
+  $DOSBOX_EXE dosbox -conf $DOSBOX_BASE -conf $DOSBOX_AUTOEXEC
   wait $!
   
-  rm $DOSBOX_AUTOEXEC 
+  rm $DOSBOX_AUTOEXEC
+  unset SDL_MOUSE_RELATIVE
+}
+
+function dosbox_conf( )
+{
+  check_editor
+  
+  if [ ! -z "$_EDITOR" ]; then
+    $_EDITOR $DOSBOX_BASE
+    proc_cont
+  else
+    fatal "Not found a text editor. Set You text editor in environment value \'VISUAL\' or \'EDITOR\', or instal nano editor."
+  fi
+
+}
+
+function game_run( )
+{
+  if [ $STORAGE_LOAD_G -eq 0 ] && [ "$GAME_CONFIG_FILE" ]; then
+     dosbox_run $CONFIG_EXEC_FILE 
+  fi   
+  dosbox_run $GAME_EXEC_FILE
 }
 
 function proc_cont( )
 {
-     message "Press [c] and [ENTER] to continue or only [ENTER] for quit"
+   question "Run $GAME_NAME now?"
+   if [[ $? -eq 1 ]]; then
+     game_run
+   fi  
+}
 
-   read INP
-   if [ "$INP" ==  "c" ]; then
-     dosbox_run "$GAME_EXEC"
-   else
-     exit
-   fi
+function help 
+{
+cat << END_ARGS
+  No arguments        This same like --play
+  --setup or -s       Edit primary Dosbox configurations
+  --configure or -c   Lauch the game configurator
+  --play  or -p       Starting this game.
+END_ARGS
 }
 
 ### MAIN ###
-[ -z "$GAME_EXEC" ] && fatal "Toto je chyba vyvoje: NENASTAVEN SPUSTITELNY SOUBOR HRY!"
+
 
 case "$1" in
-    settings) 
-      if [ -z "$GAME_CONFIG" ]; then
-         message "Tato hra nenabizi zadnou vlastni konfiguraci"
-      else
-       dosbox_run "$GAME_CONFIG"
-      fi
-      proc_cont
+    --help|-h)
+      help
     ;;
-    setup)
-      [ -z "$EDITOR" ] && EDITOR=/bin/nano
-       $EDITOR $DOSBOX_BASE
-       proc_cont
+    
+    --setup|-s)
+      dosbox_conf
     ;;
-    play|*)	
-      dosbox_run "$GAME_EXEC"
+
+    --configure|-c)
+      dosbox_run $CONFIG_EXEC_FILE 
+      proc_cont 	
+    ;;
+
+#    --info|-i)
+#      dosbox_run $MANUAL_EXEC_FILE 
+#      proc_cont 	
+#    ;;
+    
+    --play|*)
+      game_run	
     ;;
 esac
 
-
+### END ###
