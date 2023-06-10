270436b8d234c6ef8eb8418ded5395a539599bad
D.A.Tiger <drakarax@seznam.cz>
2023-06-10 23:15:14 +0200

    A few fixed for lunching the DOSBox

--- project/selfexec/Data/.tools/go.sh
+++ project/selfexec/Data/.tools/go.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
-#DOSBOX_EXEC=$(which dosbox)
-DOSBOX_BASE=${GAME_ROOT}/${DRIVE_DIR}/dosbox.conf
-DOSBOX_AUTOEXEC=${GAME_ROOT}/autoexec.conf
-
-# GAME_EXEC_FILE="DEFAULT.EXE"
-# GAME_CONFIG_FILE="CONFIGURE.EXE"
-# MANUAL_EXEC_FILE="MAUAL.EXE"
-
-#_EDITOR=""
-
-### Pomocne funkce ###
+### Operations ##############################
 function check_editor( )
 {
   if [ -n "$VISUAL" ]; then
@@ -45,13 +35,13 @@ EOF
 
 function dosbox_run( )
 {
-  autoexec_gen $1
+  autoexec_gen "$1"
   
   export SDL_MOUSE_RELATIVE=1
-  $DOSBOX_EXE dosbox -conf $DOSBOX_BASE -conf $DOSBOX_AUTOEXEC
+  $DOSBOX_EXEC -conf "$DOSBOX_BASE" -conf "$DOSBOX_AUTOEXEC"
   wait $!
   
-  rm $DOSBOX_AUTOEXEC
+  rm "$DOSBOX_AUTOEXEC"
   unset SDL_MOUSE_RELATIVE
 }
 
@@ -60,7 +50,7 @@ function dosbox_conf( )
   check_editor
   
   if [ ! -z "$_EDITOR" ]; then
-    $_EDITOR $DOSBOX_BASE
+    $_EDITOR "$DOSBOX_BASE"
     proc_cont
   else
     fatal "Not found a text editor. Set You text editor in environment value \'VISUAL\' or \'EDITOR\', or instal nano editor."
@@ -71,9 +61,9 @@ function dosbox_conf( )
 function game_run( )
 {
   if [ $STORAGE_LOAD_G -eq 0 ] && [ "$GAME_CONFIG_FILE" ]; then
-     dosbox_run $CONFIG_EXEC_FILE 
+     dosbox_run "$CONFIG_EXEC_FILE" 
   fi   
-  dosbox_run $GAME_EXEC_FILE
+  dosbox_run "$GAME_EXEC_FILE"
 }
 
 function proc_cont( )
@@ -89,13 +79,15 @@ function help
 cat << END_ARGS
   No arguments        This same like --play
   --setup or -s       Edit primary Dosbox configurations
-  --configure or -c   Lauch the game configurator
+  --configure or -c   Run the game configurator (if it exists)
+  --info or -i        Start the game manual application (if it exists)
   --play  or -p       Starting this game.
 END_ARGS
 }
 
-### MAIN ###
-
+### MAIN ########################################
+[ -z "$DOSBOX_BASE" ] && DOSBOX_BASE="${GAME_ROOT}/${DRIVE_DIR}/dosbox.conf"
+[ -z "$DOSBOX_AUTOEXEC" ] && DOSBOX_AUTOEXEC="${GAME_ROOT}/autoexec.conf"
 
 case "$1" in
     --help|-h)
@@ -107,18 +99,26 @@ case "$1" in
     ;;
 
     --configure|-c)
-      dosbox_run $CONFIG_EXEC_FILE 
-      proc_cont 	
+      if [ "$CONFIG_EXEC_FILE" ]; then
+        dosbox_run "$CONFIG_EXEC_FILE" 
+      else
+        message "Unsupported request - No known configuration utility"
+      fi
+        proc_cont 	
     ;;
 
-#    --info|-i)
-#      dosbox_run $MANUAL_EXEC_FILE 
-#      proc_cont 	
-#    ;;
+    --info|-i)
+      if [ "$GAME_UTILITY_MANUAL" ]; then
+        dosbox_run "$GAME_UTILITY_MANUAL"
+      else
+        message "Unsupported request - No known manual utility"
+      fi
+        proc_cont 	
+    ;;
     
     --play|*)
       game_run	
     ;;
 esac
 
-### END ###
+### END #########################################
